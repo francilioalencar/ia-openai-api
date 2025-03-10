94ace502b20dfe08bdac6d0e160db60024d0732a
francilioalencar <francilioalencar@gmail.com>
2025-03-09 23:02:47 -0300

    Inclusao de endpoint para receber imagem e gerar sua descricao

--- src/main/java/com/francilio/api/controller/SpringAIChatClientApiController.java
+++ src/main/java/com/francilio/api/controller/SpringAIChatClientApiController.java
@@ -1,14 +1,29 @@
 package com.francilio.api.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.francilio.api.dto.Prompt;
 import com.francilio.api.service.SpringAIChatClientApiService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/chatClientOpenAi")
@@ -32,5 +47,36 @@ public class SpringAIChatClientApiController {
         return springAIChatClientApiService.obterResposeChatClient(prompt.system(), prompt.user());
 
     }
+
+    @Operation(
+        summary = "Faz upload de uma imagem e retorna uma descrição",
+        description = "Este endpoint recebe uma imagem < 1M e retorna uma descrição detalhada gerada por IA."
+        
+    )
+    @ApiResponse(responseCode = "200", description = "Descrição gerada com sucesso")
+    @ApiResponse(responseCode = "500", description = "Erro ao processar a imagem")
+    @PostMapping(value = "/descrever-imagem", consumes = "multipart/form-data")
+
+    //@PostMapping("/img")
+    public ResponseEntity<String> obterDescricaoDeImagem(@RequestParam("imagem") MultipartFile imagem) {
+
+        try {
+            // 1 Salvar a imagem temporariamente
+            File arquivoTemp = File.createTempFile("upload-", ".jpg");
+            try (FileOutputStream fos = new FileOutputStream(arquivoTemp)) {
+                fos.write(imagem.getBytes());
+            }
+
+            // 2️ Criar um Resource da imagem salva
+            Resource imagemResource = new FileSystemResource(arquivoTemp);
+
+            String str="""
+            Realize uma breve descrição detalada da imagem enviada.        
+            """;
+        return ResponseEntity.ok(  springAIChatClientApiService.obterDescreveImagem(str, str, imagemResource) );
+        } catch (IOException e) {
+                    return ResponseEntity.status(500).body("Erro ao processar a imagem.");
+                }
+            }
 
 }
--- src/main/java/com/francilio/api/service/SpringAIChatClientApiService.java
+++ src/main/java/com/francilio/api/service/SpringAIChatClientApiService.java
@@ -1,8 +1,15 @@
 package com.francilio.api.service;
 
+import java.util.List;
+
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.model.Media;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+
 
 @Service
 public class SpringAIChatClientApiService {
@@ -23,6 +30,21 @@ public class SpringAIChatClientApiService {
                     .temperature(0.8)
                     .build()
                 )
+                .call()
+                .content();
+    }
+
+    public String obterDescreveImagem(String promptSystem, String promptUser, Resource imagem) {
+
+        Media media = new Media(MimeTypeUtils.IMAGE_JPEG, imagem);
+        UserMessage userMessage = new UserMessage(promptUser, List.of(media));
+
+
+
+        return this.chatClient.prompt()
+                .system(promptSystem)
+                .messages(List.of(userMessage))
+                .options(ChatOptions.builder().temperature(0.8).build())
                 .call()
                 .content();
     }
